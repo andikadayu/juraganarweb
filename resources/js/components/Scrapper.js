572b290db7a3c9018082ab66ddb9db5f436b1814
Andika Dayu <dikapolk@gmail.com>
2022-06-05 11:46:23 +0700

    Add Progress on Scrapping Process

--- resources/js/components/Scrapper.js
+++ resources/js/components/Scrapper.js
@@ -1,59 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Swal from 'sweetalert2';
 
 
 class Scrapper extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { loading: 0 }
+        this.state = { loading: 0, isSubmit: false }
         // This binding is necessary to make `this` work in the callback
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.saveLoading = this.saveLoading.bind(this);
+    }
+
+    saveLoading(value) {
+        this.setState({ loading: value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        let formData = new FormData(document.querySelector("form[id='formScrap']"));
-        Swal.showLoading();
-        axios({
-            method: "post",
-            url: "/api/scrapping",
-            data: formData,
-            responseType: 'json'
-        }).then(function (response) {
-            Swal.close();
-            console.log(response);
-            if (response.data.status == 'OK') {
-                Swal.fire({
-                    title: 'Success',
-                    text: response.data.result,
-                    timer: 5000,
-                    icon: 'success',
-                });
-                location.reload();
-            } else {
-                Swal.fire({
-                    title: 'Failed',
-                    text: response.data.result,
-                    timer: 5000,
-                    icon: 'error',
-                });
-                location.reload();
-            }
 
-        }).catch(function (error) {
-            Swal.close();
-            console.log(error);
-            Swal.fire({
-                title: 'Failed',
-                text: 'Scrap has Error Occured',
-                timer: 5000,
-                icon: 'error',
-            });
-            // location.reload();
-        });
+        this.saveLoading(0);
+        this.setState({ isSubmit: true });
+
+        let formData = new FormData(document.querySelector("form[id='formScrap']"));
+        let links = formData.get('shopeelink');
+        let _token = formData.get('_token');
+        let id_user = formData.get('id_user');
+
+        let allLink = links.split(',');
+
+        let count = allLink.length;
+        let current = 1;
+
+        for (let index = 0; index < count; index++) {
+            const mylink = allLink[index];
+            let formNew = new FormData();
+            formNew.append('shopeelink', mylink);
+            formNew.append('_token', _token);
+            formNew.append('id_user', id_user);
+
+            setTimeout(async () => {
+                await axios({
+                    method: "post",
+                    url: "/api/scrapping",
+                    data: formNew,
+                    responseType: 'json'
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
+                })
+                this.saveLoading(current / count * 100);
+                current++;
+
+            }, 2000);
+
+        }
+
 
     }
 
@@ -64,18 +68,18 @@ class Scrapper extends React.Component {
                     <div className="card">
                         <div className="card-body">
                             <form id='formScrap' onSubmit={this.handleSubmit}>
-                                <input className='form-control' type={'hidden'} id='csrf_token_field' name='_token'></input>
-                                <input className='form-control' type={'hidden'} id='user_id_field' name='id_user'></input>
+                                <input className='form-control' type={'hidden'} id='csrf_token_field' name='_token' disabled={this.state.isSubmit}></input>
+                                <input className='form-control' type={'hidden'} id='user_id_field' name='id_user' disabled={this.state.isSubmit}></input>
 
                                 <div className="form-group">
                                     <label className="form-label">Link Shopee</label>
                                     <textarea name="shopeelink" id="shopeelink" className="form-control"
-                                        placeholder="Enter link separate by comma" required style={{ height: 200 + 'px' }}></textarea>
+                                        placeholder="Enter link separate by comma" required style={{ height: 200 + 'px' }} disabled={this.state.isSubmit}></textarea>
                                 </div>
 
                                 <div className="d-flex justify-content-between mt-5">
                                     <a href="#" id='btnBack' className="btn btn-secondary">Back</a>
-                                    <button type="submit" className="btn btn-success">Save</button>
+                                    <button type="submit" className="btn btn-success" disabled={this.state.isSubmit}>Save</button>
                                 </div>
 
                             </form>
